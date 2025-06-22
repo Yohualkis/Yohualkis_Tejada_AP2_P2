49da184a23b89d60f8acd9a855af5a4204802159
Yohualkis <Yohualkis99@gmail.com>
2025-06-22 01:51:25 -0400

    Se agrego el decorador @AndroidEntryPoint (hiltInjectionDependency) y se invoca el navhost para correr la app

--- app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/MainActivity.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/MainActivity.kt
@@ -7,41 +7,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.ucne.yohualkis_tejada_ap2_p2.presentation.navigation.GeneralNavHost
 import com.ucne.yohualkis_tejada_ap2_p2.ui.theme.Yohualkis_Tejada_AP2_P2Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Yohualkis_Tejada_AP2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val nav = rememberNavController()
+                Scaffold { innerPadding ->
+                    GeneralNavHost(
+                        nav,
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Yohualkis_Tejada_AP2_P2Theme {
-        Greeting("Android")
     }
 }
