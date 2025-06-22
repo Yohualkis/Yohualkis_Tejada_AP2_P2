a4851e739bb0a01385b0a04be7f6910fbb968cbb
Yohualkis <Yohualkis99@gmail.com>
2025-06-22 01:49:37 -0400

    Se creo el navhost

---
+++ app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/presentation/navigation/GeneralNavHost.kt
@@ -0,0 +1,27 @@
+package com.ucne.yohualkis_tejada_ap2_p2.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.ucne.yohualkis_tejada_ap2_p2.presentation.repositorio.ClientesListScreen
+
+@Composable
+fun GeneralNavHost(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.ListaEntidades,
+        modifier = modifier
+    ) {
+        composable<Screen.ListaEntidades>{
+            ClientesListScreen(
+                nuevoCliente = {},
+                goBack = { navHostController.navigateUp() }
+            )
+        }
+    }
+}
