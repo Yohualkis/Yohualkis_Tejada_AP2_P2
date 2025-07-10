c472d912d42158d2b584d657427882251f4b1255
Yohualkis <Yohualkis99@gmail.com>
2025-07-09 22:20:10 -0400

    Se agrego la funcion de obtener los contribuyentes

--- app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/data/remote/RemoteDataSource.kt
@@ -4,7 +4,13 @@ import com.ucne.yohualkis_tejada_ap2_p2.data.remote.dto.RepositorioDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
-    private val gitHubApi: GitHubApi
+    private val gitHubApi: GitHubApi,
+    private val contributorApi: ContributorApi
 ){
+    // Repositorios
     suspend fun getRepositorios(username: String): List<RepositorioDto> = gitHubApi.listRepos(username)
+
+    // Contribuyentes
+    suspend fun getContributors(username: String, nombreRepo: String) =
+        contributorApi.getContributors(username,nombreRepo)
 }
