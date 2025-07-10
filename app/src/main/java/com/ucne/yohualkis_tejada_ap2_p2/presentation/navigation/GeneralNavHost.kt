415dd0af6d05065ad49980fda5c076b09cadc3db
Yohualkis <Yohualkis99@gmail.com>
2025-07-09 22:19:52 -0400

    Se modifico el navhost agregando las pantallas correspondientes

--- app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/presentation/navigation/GeneralNavHost.kt
+++ app/src/main/java/com/ucne/yohualkis_tejada_ap2_p2/presentation/navigation/GeneralNavHost.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.ucne.yohualkis_tejada_ap2_p2.presentation.contributors.ContributorsListScreen
 import com.ucne.yohualkis_tejada_ap2_p2.presentation.repositorio.RepositorioListScreen
 
 @Composable
@@ -14,13 +16,29 @@ fun GeneralNavHost(
 ) {
     NavHost(
         navController = navHostController,
-        startDestination = Screen.ListaEntidades,
+        startDestination = Screen.ListaRepositorios,
         modifier = modifier
     ) {
-        composable<Screen.ListaEntidades>{
+
+        // LISTA REPOSITORIOS
+        composable<Screen.ListaRepositorios>{
             RepositorioListScreen(
                 nuevoRepo = {},
-                goBack = { navHostController.navigateUp() }
+                goBack = { navHostController.navigateUp() },
+                goToContributor = { username, nombreRepo ->
+                    navHostController.navigate(Screen.ListaContributors(username, nombreRepo))
+                }
+            )
+        }
+
+        // LISTA CONTRIBUYENTES
+        composable<Screen.ListaContributors> {
+            val args = it.toRoute<Screen.ListaContributors>()
+            ContributorsListScreen(
+                nombreRepo = args.nombreRepo,
+                goBack = {
+                    navHostController.navigateUp()
+                },
             )
         }
     }
